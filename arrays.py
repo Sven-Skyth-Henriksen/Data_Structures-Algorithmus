583e9b5ffb0600d2925c6810328b135bb8038cd9
Sven Skyth Henriksen <sven.skyth.henriksen@gmail.com>
2021-09-13 20:25:32 +0200

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -1,4 +1,24 @@
 new_list = [1, 2, 3]
 result = new_list[0]
 
-print(result)
+
+if 1 in new_list:
+	print(True)
+
+for n in new_list:
+	if n == 1:
+		print(True)
+
+		break
+
+
+numbers =[]
+print(len(numbers))
+
+numbers.append(2)
+print(len(numbers))
+
+numbers.extend([3, 4, 5])
+print(numbers)
+
+
