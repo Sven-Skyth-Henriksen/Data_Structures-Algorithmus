d5456a49f25773a50da221c4b268a1fe055c4c36
Sven Skyth Henriksen <sven.skyth.henriksen@gmail.com>
2021-09-14 09:44:31 +0200

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -14,3 +14,49 @@ def merge_sort(list):
 	left_half, right_half = split(list)
 	left = merge_sort(left_half)
 	right = merge_sort(right_half)
+
+	return merge(left, right)
+
+def split(list):
+	"""
+	Divide the unsorted list at midpoint into sublists
+	Returns two sublists - left and right 
+	"""
+
+	mid = len(list) // 2
+	left = list[:mid]
+	right = list[mid:]
+
+	return left, right 
+
+
+def merge(left, right):
+	"""
+	Merge two lists (arrays), sorting them in the process
+	Returns a new merged list
+	"""
+	l = []
+	i = 0
+	j = 0
+
+	while i < len(left) and j < len(right):
+		if left[i] < right[j]:
+			l.append(left[i])
+			i += 1
+		else:
+			l.append(right[j])
+			j += 1
+
+	while i < len(left):
+		l.append(left[i])
+		i += 1
+
+	while j < len(right):
+		l.append(right[j])
+		j += 1
+
+	return l
+
+
+alist = [54,26,93,17,77,31,44,55,20]
+print(merge_sort(alist))
